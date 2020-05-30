a6ba5895d0d1a101f5e9c22bdafcddb03f8f3ff2
V.S <v.s@VSs-New-MacBook-Air.local>
2020-05-30 12:23:08 -0400

    got word count working

---
+++ W5_May_23_2020/in_class/inClass_ex3.scala
@@ -0,0 +1,24 @@
+import org.apache.log4j._
+import org.apache.spark.sql.SparkSession
+object wordCount {
+  def main(args: Array[String]) {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val session = SparkSession
+      .builder
+      .master("local[2]")
+      .appName("wordCount")
+      .getOrCreate()
+    val input = session.read.textFile("TheHungerGames.txt")
+    import session.implicits._
+    val words = input.flatMap(x => x.split(" "))
+  //println(words.getClass)
+/*	
+    val wordCounts = words.rdd.countByValue()
+    wordCounts.foreach(println)
+  }
+  def clean(s: String): String =
+  {
+    s.replaceAll("[^\\w\\s]", "")
+  }
+*/
+}
